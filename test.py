fbf903c6a1a9679cacdef2d654b484c00af10b33
saikaldi <YOUR EMAIL ON EMAIL>
2023-09-27 12:47:06 -0700

    First Commit

---
+++ test.py
@@ -0,0 +1,7 @@
+from termcolor import cprint
+import emoji
+
+print('Hello from CMD')
+
+cprint('hello, world', 'green', 'on_red', attrs=["bold", "underline"])
+print(emoji.emojize('Python is :thumbs_up:'))
